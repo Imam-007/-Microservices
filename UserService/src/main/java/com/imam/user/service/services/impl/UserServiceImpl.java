18782396ecc3086aec06de19466636b31e0acb95
Imam-IBEE <imam@ibeesolutions.com>
2024-10-24 13:06:27 +0530

    added code for showing rating for every user

--- UserService/src/main/java/com/imam/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/imam/user/service/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.imam.user.service.services.impl;
 
 import com.imam.user.service.exceptions.ResourceNotFoundException;
+import com.imam.user.service.model.Rating;
 import com.imam.user.service.model.User;
 import com.imam.user.service.repositories.UserRepository;
 import com.imam.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,10 +20,16 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Override
     public User saveUser(User user) {
 
-        String randomUserId= UUID.randomUUID().toString();
+        String randomUserId = UUID.randomUUID().toString();
         user.setUserId(randomUserId);
         return userRepository.save(user);
     }
@@ -30,6 +41,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User Not Found "+userId));
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not Found " + userId));
+        ArrayList<Rating> ratingOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{}", ratingOfUser);
+        user.setRatings(ratingOfUser);
+
+        return user;
     }
 }
